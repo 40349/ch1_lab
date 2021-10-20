efd30a9d4763e928d5476d2b46b5fe838663e349
40349 <kalyclaire@gmail.com>
2021-10-20 18:50:33 +0800

    first

---
+++ Lab1/source/Main.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	printf("hi");
+	system("pause");
+	return 0;
+}
---
+++ Lab2/source/user_defined.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+int user_defined_print(void)
+{
+	printf("welcome to c!\n");
+	system("pause");
+	return 0;
+}
